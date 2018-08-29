c831ff8e76e1899d1ef0dade5645b947fcd47f8a
hoojo <hoojo@qq.com>
2018-08-29 22:50:57 +0800

    :sparkles: :bento:  Added  python3 `import->test1` example

--- 11_examples_import/test1.sh
+++ 11_examples_import/test1.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-06-12 21:06:35
+# @copyright by hoojo@2018
+# @changelog Added python3 `import->test1` example
 
 url="http://www.baidu.com"
