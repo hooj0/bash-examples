88a622965e2e7dfc988e59ddcc3494df6e2be321
hoojo <hoojo@qq.com>
2018-08-29 22:51:00 +0800

    :sparkles: :bento:  Added  python3 `settings->settings ifs` example

--- 13_examples_settings/settings_ifs.sh
+++ 13_examples_settings/settings_ifs.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
-# author:   hoojo
-# email:    hoojo_@126.com
-# github:   https://github.com/hooj0
-# create date: 2018-06-28
-# copyright by hoojo @ 2018
+#!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-07-01 15:34:30
+# @copyright by hoojo@2018
+# @changelog Added python3 `settings->settings ifs` example
 
 
 
