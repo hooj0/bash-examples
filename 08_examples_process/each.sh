926951b573ae6710b326ab6206aec13d9171aaf1
hoojo <hoojo@qq.com>
2018-08-29 22:50:39 +0800

    :sparkles: :bento:  Added  python3 `process->each` example

--- 08_examples_process/each.sh
+++ 08_examples_process/each.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-05-29 22:44:12
+# @copyright by hoojo@2018
+# @changelog Added python3 `process->each` example
 
 my_arry=(a b "c","d" abc)
 echo "-------FOR循环遍历输出数组--------"
