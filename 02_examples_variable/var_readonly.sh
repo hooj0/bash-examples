73c4dd99d391ab64b10009c87a0066034230b680
hoojo <hoojo@qq.com>
2018-08-29 22:50:15 +0800

    :sparkles: :bento:  Added  python3 `variable->var readonly` example

--- 02_examples_variable/var_readonly.sh
+++ 02_examples_variable/var_readonly.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-05-27 16:04:25
+# @copyright by hoojo@2018
+# @changelog Added python3 `variable->var readonly` example
 
 ##################################################################
 #							只读变量
