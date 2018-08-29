afa61db013a94716e4244b8a5140efb136d878c0
hoojo <hoojo@qq.com>
2018-08-29 22:50:12 +0800

    :sparkles: :bento:  Added  python3 `variable->var assignment` example

--- 02_examples_variable/var_assignment.sh
+++ 02_examples_variable/var_assignment.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-06-03 09:52:32
+# @copyright by hoojo@2018
+# @changelog Added python3 `variable->var assignment` example
 
 # ======================================================================
 #					变量赋值操作
