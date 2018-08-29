f68063c7251af534867a0ee23302660b3858bf01
hoojo <hoojo@qq.com>
2018-08-29 22:50:54 +0800

    :sparkles: :bento:  Added  python3 `file->file stdin` example

--- 10_examples_file/file_stdin.sh
+++ 10_examples_file/file_stdin.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-06-07 21:16:39
+# @copyright by hoojo@2018
+# @changelog Added python3 `file->file stdin` example
 
 # ======================================================================
 #					Shell 输入/输出重定向————输入重定向
