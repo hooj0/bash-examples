a1ca68ff91ee913de4542cd22d1c53d88cf9548e
hoojo <hoojo@qq.com>
2018-08-29 22:50:33 +0800

    :sparkles: :bento:  Added  python3 `output->echo output` example

--- 06_examples_output/echo_output.sh
+++ 06_examples_output/echo_output.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-05-30 20:59:40
+# @copyright by hoojo@2018
+# @changelog Added python3 `output->echo output` example
 
 
 # ===================================================================
