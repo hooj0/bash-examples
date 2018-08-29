a01ae67ad9c52a94c91baa6b7ad7618151caf501
hoojo <hoojo@qq.com>
2018-08-29 22:50:44 +0800

    :sparkles: :bento:  Added  python3 `process->process loop jump` example

--- 08_examples_process/process_loop_jump.sh
+++ 08_examples_process/process_loop_jump.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-06-05 20:52:49
+# @copyright by hoojo@2018
+# @changelog Added python3 `process->process loop jump` example
 
 # ======================================================================
 #					流程语句———— 跳出循环
