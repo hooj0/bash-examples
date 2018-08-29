14a5cc30568ef3d97320f49afe354dd3d432be6c
hoojo <hoojo@qq.com>
2018-08-29 22:50:35 +0800

    :sparkles: :bento:  Added  python3 `output->printfc output` example

--- 06_examples_output/printfc_output.sh
+++ 06_examples_output/printfc_output.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-05-30 22:55:48
+# @copyright by hoojo@2018
+# @changelog Added python3 `output->printfc output` example
 
 # ===========================================================
 #			printf的转义序列
