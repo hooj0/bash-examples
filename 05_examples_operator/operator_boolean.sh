5fd78007dabf3bcc4bfe09ece78617ff713007b9
hoojo <hoojo@qq.com>
2018-08-29 22:50:27 +0800

    :sparkles: :bento:  Added  python3 `operator->operator boolean` example

--- 05_examples_operator/operator_boolean.sh
+++ 05_examples_operator/operator_boolean.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-05-29 21:08:48
+# @copyright by hoojo@2018
+# @changelog Added python3 `operator->operator boolean` example
 
 #---------------------------------------------------------------------
 #				布尔运算符
