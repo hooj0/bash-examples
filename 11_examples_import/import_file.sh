baca2c2ed23d0622b502f066f27441e9e1f0494a
hoojo <hoojo@qq.com>
2018-08-29 22:50:56 +0800

    :sparkles: :bento:  Added  python3 `import->import file` example

--- 11_examples_import/import_file.sh
+++ 11_examples_import/import_file.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-06-12 21:01:18
+# @copyright by hoojo@2018
+# @changelog Added python3 `import->import file` example
 
 # ======================================================================
 #					Shell 文件导入
