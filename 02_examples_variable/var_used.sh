fe49152b6b2259ffc93cb20bc77ddf3fa1fa018f
hoojo <hoojo@qq.com>
2018-08-29 22:50:18 +0800

    :sparkles: :bento:  Added  python3 `variable->var used` example

--- 02_examples_variable/var_used.sh
+++ 02_examples_variable/var_used.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-05-27 13:05:47
+# @copyright by hoojo@2018
+# @changelog Added python3 `variable->var used` example
 
 ###############################################################
 #					使用变量
