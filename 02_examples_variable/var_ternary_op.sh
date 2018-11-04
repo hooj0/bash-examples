95a3eb3b41937d2c297ec3e1ec882fc178048c8e
hoojo <hoojo@qq.com>
2018-11-04 12:50:27 +0800

    :sparkles: :bento:  Added  bash `variable->var ternary op` example

--- 02_examples_variable/var_ternary_op.sh
+++ 02_examples_variable/var_ternary_op.sh
@@ -4,7 +4,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-06-12 23:03:00
 # @copyright by hoojo@2018
-# @changelog Added python3 `variable->var ternary op` example
+# @changelog Added bash `variable->var ternary op` example
 
 # ======================================================================
 #					变量三元运算
