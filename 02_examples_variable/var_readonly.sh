b9a97d1d334f6c3f7078a107a143fd8c2a651929
hoojo <hoojo@qq.com>
2018-11-04 12:50:26 +0800

    :sparkles: :bento:  Added  bash `variable->var readonly` example

--- 02_examples_variable/var_readonly.sh
+++ 02_examples_variable/var_readonly.sh
@@ -4,19 +4,19 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-05-27 16:04:25
 # @copyright by hoojo@2018
-# @changelog Added python3 `variable->var readonly` example
+# @changelog Added bash `variable->var readonly` example
 
 ##################################################################
 #							只读变量
 ##################################################################
 
 # 使用 readonly 命令可以将变量定义为只读变量，只读变量的值不能被改变。
-myUrl="http://www.w3cschool.cc"
+myUrl="https://github.com/hooj0"
 # 设置为只读
 readonly myUrl
 
 # 下面的例子尝试更改只读变量，结果报错：
-myUrl="http://www.runoob.com"
+myUrl="https://hooj0.github.io/"
 
 # 运行脚本，结果如下：
 # /bin/sh: NAME: This variable is read only.
