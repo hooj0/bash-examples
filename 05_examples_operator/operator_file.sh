fedb59f41ec7a8c9013d2a2050f959a55ca4747a
hoojo <hoojo@qq.com>
2018-11-14 14:36:59 +0800

    :sparkles: :bento:  Added  bash `operator->operator file` example

--- 05_examples_operator/operator_file.sh
+++ 05_examples_operator/operator_file.sh
@@ -34,6 +34,32 @@
 # file1 -nt file2     # file1 比 file2 更新
 # file1 -ot file2     # file1 比 file2 更老
 #----------------------------------------------------------------------
+# 测试文件表达式
+# ==============================================================================
+# 表达式   	        如果下列条件为真则返回True
+# ------------------------------------------------------------------------------
+# file1 -ef file2 	file1 和 file2 拥有相同的索引号（通过硬链接两个文件名指向相同的文件）。
+# file1 -nt file2 	file1新于 file2。
+# file1 -ot file2 	file1早于 file2。
+# -b file 	        file 存在并且是一个块（设备）文件。
+# -c file 	        file 存在并且是一个字符（设备）文件。
+# -d file 	        file 存在并且是一个目录。
+# -e file 	        file 存在。
+# -f file 	        file 存在并且是一个普通文件。
+# -g file 	        file 存在并且设置了组 ID。
+# -G file 	        file 存在并且由有效组 ID 拥有。
+# -k file 	        file 存在并且设置了它的“sticky bit”。
+# -L file 	        file 存在并且是一个符号链接。
+# -O file 	        file 存在并且由有效用户 ID 拥有。
+# -p file 	        file 存在并且是一个命名管道。
+# -r file 	        file 存在并且可读（有效用户有可读权限）。
+# -s file 	        file 存在且其长度大于零。
+# -S file 	        file 存在且是一个网络 socket。
+# -t fd 	        fd 是一个定向到终端／从终端定向的文件描述符 。 这可以被用来决定是否重定向了标准输入／输出错误。
+# -u file 	        file 存在并且设置了 setuid 位。
+# -w file 	        file 存在并且可写（有效用户拥有可写权限）。
+# -x file 	        file 存在并且可执行（有效用户有执行／搜索权限）。
+# ------------------------------------------------------------------------------
 
 
 #----------------------------------------------------------------------
@@ -41,60 +67,52 @@
 #----------------------------------------------------------------------
 # 变量 file 表示文件"./operator_file.sh"，它的大小为100字节，具有 rwx 权限。下面的代码，将检测该文件的各种属性：
 #----------------------------------------------------------------------
-
 file="./operator_file.sh"
 
 
-if [ -r $file ]
-then
+if [ -r $file ]; then
    echo "文件可读"
 else
    echo "文件不可读"
 fi
 
 
-if [ -w $file ]
-then
+if [ -w $file ]; then
    echo "文件可写"
 else
    echo "文件不可写"
 fi
 
 
-if [ -x $file ]
-then
+if [ -x $file ]; then
    echo "文件可执行"
 else
    echo "文件不可执行"
 fi
 
 
-if [ -f $file ]
-then
+if [ -f $file ]; then
    echo "文件为普通文件"
 else
    echo "文件为特殊文件"
 fi
 
 
-if [ -d $file ]
-then
+if [ -d $file ]; then
    echo "文件是个目录"
 else
    echo "文件不是个目录"
 fi
 
 
-if [ -s $file ]
-then
+if [ -s $file ]; then
    echo "文件不为空"
 else
    echo "文件为空"
 fi
 
 
-if [ -e $file ]
-then
+if [ -e $file ]; then
    echo "文件存在"
 else
    echo "文件不存在"
@@ -124,4 +142,30 @@ fi
 # 文件不为空
 # 文件存在
 
-read exits
+
+
+# =================================================================
+# 示例：实例演示判断文件
+# =================================================================
+FILE=~/.bashrc
+if [ -e "$FILE" ]; then
+    if [ -f "$FILE" ]; then
+        echo "$FILE is a regular file."
+    fi
+    if [ -d "$FILE" ]; then
+        echo "$FILE is a directory."
+    fi
+    if [ -r "$FILE" ]; then
+        echo "$FILE is readable."
+    fi
+    if [ -w "$FILE" ]; then
+        echo "$FILE is writable."
+    fi
+    if [ -x "$FILE" ]; then
+        echo "$FILE is executable/searchable."
+    fi
+else
+    echo "$FILE does not exist"
+    exit 1
+fi
+exit
