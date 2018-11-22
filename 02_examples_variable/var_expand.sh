8ec6db24f620d36531267718ce6f3412ecbe4857
hoojo <hoojo@qq.com>
2018-11-22 15:05:24 +0800

    :sparkles: :bento:  Added  bash `variable->var expand` example

--- 02_examples_variable/var_expand.sh
+++ 02_examples_variable/var_expand.sh
@@ -4,21 +4,31 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-06-12 23:03:00
 # @copyright by hoojo@2018
-# @changelog Added bash `variable->var ternary op` example
+# @changelog Added bash `variable->var expand` example
+
+
 
 # ======================================================================
-#					变量三元运算
+#					变量展开
 # ======================================================================
+
+
+
+# ----------------------------------------------------------------------
 # Bash 也支持三元运算符，如下面的例子所示：
-
+# ----------------------------------------------------------------------
 ${varname:-word}    # 如果 varname 存在而且不为 null，返回它的值，否则返回 word
 ${varname:=word}    # 如果 varname 存在而且不为 null，返回它的值，否则把word赋值给它并且返回 word
 ${varname:+word}    # 如果 varname 存在而且不为 null，返回 word，否则返回 null
 ${varname:offset:length}    # 它返回 $varname 的子字符串，从 offset 处开始，长度为 length
 
+
+
+# ----------------------------------------------------------------------
+# 示例：${parameter:?word}
+# ----------------------------------------------------------------------
 # 若 parameter 没有设置或为空，这种展开导致脚本带有错误退出，并且 word 的内容会发送到标准错误。
 # 若 parameter 不为空， 展开结果是 parameter 的值。
-${parameter:?word}
 
 foo=
 echo ${foo:?"parameter is empty"}   # bash: foo: parameter is empty
@@ -27,3 +37,18 @@ echo $?     # 1
 foo=bar
 echo ${foo:?"parameter is empty"}   # bar
 echo $?     # 0
+
+
+
+# ----------------------------------------------------------------------
+# 示例：返回变量名的参数展开，${!prefix*} 或 ${!prefix@}
+# ----------------------------------------------------------------------
+# 这种展开会返回以 prefix 开头的已有变量名
+echo ${!BASH*}
+
+
+# 输出：
+# ----------------------------------------------------------------------
+#BASH BASH_ARGC BASH_ARGV BASH_COMMAND BASH_COMPLETION
+#BASH_COMPLETION_DIR BASH_LINENO BASH_SOURCE BASH_SUBSHELL
+#BASH_VERSINFO BASH_VERSION
