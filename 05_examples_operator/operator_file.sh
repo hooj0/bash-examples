6a265bb220008bb48d65e3602090ac92e4cf39b2
hoojo <hoojo@qq.com>
2019-10-20 23:05:55 +0800

    :sparkles: :bento:  Added  bash `operator -> file` example

--- 05_examples_operator/operator_file.sh
+++ 05_examples_operator/operator_file.sh
@@ -4,7 +4,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-05-29 21:09:07
 # @copyright by hoojo@2018
-# @changelog Added bash `operator->operator file` example
+# @changelog Added bash `operator -> file` example
 
 
 #------------------------------------------------------------------------------------------------------------
