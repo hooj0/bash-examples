7c343c58ac2c4d250e43e39d46c6e97b0147625d
hoojo <hoojo@qq.com>
2019-10-21 05:42:55 +0800

    :sparkles: :bento:  Added  bash `import -> shell` example

--- 11_examples_import/import_shell.sh
+++ 11_examples_import/import_shell.sh
@@ -4,7 +4,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-06-12 21:01:18
 # @copyright by hoojo@2018
-# @changelog Added bash `import->import file` example
+# @changelog Added bash `import -> shell` example
 
 # ======================================================================
 #					Shell 文件导入
@@ -27,25 +27,25 @@
 
 # 创建两个 shell 脚本文件。
 
-# test1.sh 代码如下：
+# simple.sh 代码如下：
 # url="http://www.baidu.com"
 
 
-# test2.sh 代码如下：
+# simple2.sh 代码如下：
 
-#使用  “.” 号来引用 test1.sh 文件
-. ./test1.sh
+#使用  “.” 号来引用 simple.sh 文件
+. simple.sh
 
 # 或者使用以下包含文件代码
-# source ./test1.sh
+# source ./simple.sh
 
 echo "官网地址：$url"
 
-# 接下来，我们为 test2.sh 添加可执行权限并执行：
-#$ chmod +x test2.sh 
-#$ ./test2.sh 
+# 接下来，我们为 simple2.sh 添加可执行权限并执行：
+#$ chmod +x simple2.sh
+#$ ./simple2.sh
 
 #官网地址：http://www.runoob.com
-#注：被包含的文件 test1.sh 不需要可执行权限。
+#注：被包含的文件 simple.sh 不需要可执行权限。
 
 read exits
